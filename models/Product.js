de084125c9cb3667e0a8fadfc08a3d0cafabad79
Rago <63511519+ragokan@users.noreply.github.com>
2020-12-30 00:38:43 +0300

    product model

---
+++ models/Product.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const ProductSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    stockCount: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Product", ProductSchema);
