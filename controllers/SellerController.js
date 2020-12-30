13703a10774e3b228def685830897c0e90686316
Rago <63511519+ragokan@users.noreply.github.com>
2020-12-30 12:56:27 +0300

    add product

--- controllers/SellerController.js
+++ controllers/SellerController.js
@@ -6,9 +6,19 @@ import ErrorObject from "../utils/ErrorObject.js";
 export const GetProducts = Async(async (req, res, next) => {
   let products;
   if (req.user.type === "admin") {
-    products = await Product.find();
+    products = await Product.find().populate("seller", ["type", "email", "fullname"]);
   } else if (req.user.type === "seller") {
     products = await Product.find({ seller: req.user._id });
   }
   res.status(200).json(products);
+});
+
+// Post /seller/products
+export const AddProduct = Async(async (req, res, next) => {
+  //   let { name, price, image, category, description, brand, stockCount } = req.body;
+  let product = await Product.create({
+    ...req.body,
+    seller: req.user._id,
+  });
+  res.status(200).json(product);
 });
--- routes/SellerRoutes.js
+++ routes/SellerRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { GetProducts } from "../controllers/SellerController.js";
+import { GetProducts, AddProduct } from "../controllers/SellerController.js";
 import { LoginRequired, SellerRequired } from "../middleware/UserAuth.js";
 
 const router = express.Router();
 
-router.route("/products").get(LoginRequired, SellerRequired, GetProducts);
+router
+  .route("/products")
+  .get(LoginRequired, SellerRequired, GetProducts)
+  .post(LoginRequired, SellerRequired, AddProduct);
 
 export default router;
