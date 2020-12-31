5155df295877331463c38963f6650ed184f1ec7c
Rago <63511519+ragokan@users.noreply.github.com>
2020-12-31 14:00:17 +0300

    email validation

--- validation/LoginValidation.js
+++ validation/LoginValidation.js
@@ -4,7 +4,8 @@ const loginValidation = (data) => {
   const schema = Joi.object({
     email: Joi.string().min(6).required().email().messages({
       "any.required": "Please enter a valid email!",
-      "string.min": "Please enter a valid email name!",
+      "string.min": "Please enter a valid email! ",
+      "string.email": "Please enter a valid email",
     }),
     password: Joi.string().min(5).required().messages({
       "any.required": "Please enter your password!",
