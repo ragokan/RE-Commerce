7518a6a020f77deb6a3ee3787a84401a41e99e9e
Rago <63511519+ragokan@users.noreply.github.com>
2020-12-31 14:00:33 +0300

    email validation for register

--- validation/RegisterValidation.js
+++ validation/RegisterValidation.js
@@ -9,6 +9,7 @@ const registerValidation = (data) => {
     email: Joi.string().min(6).required().email().messages({
       "any.required": "Please enter a valid email!",
       "string.min": "Please enter a valid email name!",
+      "string.email": "Please enter a valid email",
     }),
     password: Joi.string().min(5).required().messages({
       "any.required": "Please enter your password!",
