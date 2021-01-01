5e97bc4c8dd2bb9cfbad1a829e480a8417ae50c5
Rago <63511519+ragokan@users.noreply.github.com>
2021-01-01 17:51:34 +0300

    alert width

--- frontend/src/components/utils/ErrorObject.js
+++ frontend/src/components/utils/ErrorObject.js
@@ -16,6 +16,7 @@ const ErrorObject = ({ errors, dispatch }) => {
           closable
           onClose={() => onClose(error.id)}
           showIcon
+          className="alertObject"
         />
       ))}
     </>
