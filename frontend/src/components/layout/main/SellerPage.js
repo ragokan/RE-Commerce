2b21d302ed981144955743aa3bdf455e8ee10f09
Rago <63511519+ragokan@users.noreply.github.com>
2021-01-03 21:23:09 +0300

    seller page tabs

--- frontend/src/components/layout/main/SellerPage.js
+++ frontend/src/components/layout/main/SellerPage.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { Tabs } from "antd";
+import lokaly from "lokaly";
+const { TabPane } = Tabs;
 
 const SellerPage = () => {
-  return <>Seller Page</>;
+  const [currentPage, setCurrentPage] = useState("");
+  return (
+    <div className="container-fluid">
+      <Tabs defaultActiveKey="products" onChange={(key) => setCurrentPage(key)}>
+        <TabPane tab={lokaly("products")} key="products">
+          Products
+        </TabPane>
+        <TabPane tab={lokaly("addProduct")} key="addProduct">
+          Add Product
+        </TabPane>
+        <TabPane tab={lokaly("gain")} key="gain">
+          Gain
+        </TabPane>
+      </Tabs>
+    </div>
+  );
 };
 
 export default SellerPage;
