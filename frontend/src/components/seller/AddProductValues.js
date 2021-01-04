5f0f308e11f419d1c9d10d43abed32cbac93e105
Rago <63511519+ragokan@users.noreply.github.com>
2021-01-04 16:09:19 +0300

    validation

--- frontend/src/components/seller/AddProductValues.js
+++ frontend/src/components/seller/AddProductValues.js
@@ -20,18 +20,26 @@ const normFile = (e) => {
 const AddProductValues = () => {
   return (
     <>
-      <Form.Item label="Name" name="name" rules={[{ required: true }]}>
+      <Form.Item
+        label="Name"
+        name="name"
+        rules={[{ required: true, message: "Please enter a valid name!" }]}
+      >
         <Input />
       </Form.Item>
 
-      <Form.Item label="Price" name="price" rules={[{ required: true }]}>
+      <Form.Item
+        label="Price"
+        name="price"
+        rules={[{ required: true, message: "Please enter a price that higher than 0!" }]}
+      >
         <Input type="number" />
       </Form.Item>
 
       <Form.Item
         label="Image"
         name="image"
-        rules={[{ required: true }]}
+        rules={[{ required: true, message: "Please enter  a valid image!" }]}
         valuePropName="fileList"
         getValueFromEvent={normFile}
       >
@@ -44,19 +52,35 @@ const AddProductValues = () => {
         </Upload>
       </Form.Item>
 
-      <Form.Item label="Brand" name="brand" rules={[{ required: true }]}>
+      <Form.Item
+        label="Brand"
+        name="brand"
+        rules={[{ required: true, message: "Please enter a valid brand!" }]}
+      >
         <Input />
       </Form.Item>
 
-      <Form.Item label="Stock Count" name="stockCount" rules={[{ required: true }]}>
+      <Form.Item
+        label="Stock Count"
+        name="stockCount"
+        rules={[{ required: true, message: "Please enter a stock that higher or equal to 0!" }]}
+      >
         <Input type="number" />
       </Form.Item>
 
-      <Form.Item label="Description" name="description" rules={[{ required: true }]}>
+      <Form.Item
+        label="Description"
+        name="description"
+        rules={[{ required: true, message: "Please enter a valid description!" }]}
+      >
         <TextArea />
       </Form.Item>
 
-      <Form.Item label="Category" name="category" rules={[{ required: true }]}>
+      <Form.Item
+        label="Category"
+        name="category"
+        rules={[{ required: true, message: "Please enter a valid category!" }]}
+      >
         <Select>
           {categories.map((category, index) => (
             <Option key={index} value={category}>
