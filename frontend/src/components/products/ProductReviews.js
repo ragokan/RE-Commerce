5683aa0f69360a69aaf854e8d6b93c6042caa216
Rago <63511519+ragokan@users.noreply.github.com>
2021-01-06 13:20:44 +0300

    add review part

--- frontend/src/components/products/ProductReviews.js
+++ frontend/src/components/products/ProductReviews.js
@@ -1,19 +1,59 @@
-import { Divider } from "antd";
+import { Card, Divider, Form, Input, Button, Rate } from "antd";
 import React, { useEffect, useState } from "react";
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 12, span: 16 },
+};
 
 const ProductReviews = ({ user, product }) => {
   const [canAddReview, setCanAddReview] = useState(false);
 
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
   useEffect(() => {
+    if (!user) return;
     const index = user.purchasedProducts.findIndex((item) => item === product._id);
     if (index !== -1) setCanAddReview(true);
-  }, [user.purchasedProducts, product._id]);
+  }, [user, product._id]);
 
   return (
     <>
       <Divider orientation="left">Reviews</Divider>
       {product?.reviews?.length < 1 ? <h4>{"No reviews yet."}</h4> : <> </>}
-      {canAddReview && "Add"}
+      {canAddReview && (
+        <Card bordered title="Add Review">
+          <Form {...layout} onFinish={onFinish}>
+            <Form.Item
+              label="Rate"
+              name="rate"
+              rules={[{ required: true, message: "Please give a rate." }]}
+              initialValue={3}
+            >
+              <Rate allowClear={false} />
+            </Form.Item>
+
+            <Form.Item
+              label="Details"
+              name="details"
+              rules={[{ required: true, message: "Please give details." }]}
+            >
+              <Input.TextArea />
+            </Form.Item>
+
+            <Form.Item {...tailLayout}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
+      )}
     </>
   );
 };
