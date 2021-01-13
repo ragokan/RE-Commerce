68c7929d6a8f9b3ce42d16e50c138d2df4402312
Rago <63511519+ragokan@users.noreply.github.com>
2021-01-13 16:59:54 +0300

    adjust sizing and title

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,14 @@ import React, { Suspense } from "react";
 import Router from "./components/router/Router";
 import DataHooks from "./components/utils/DataHooks";
 import ErrorBoundary from "./ErrorBoundary";
+import Helmet from "react-helmet";
 
 function App() {
   return (
     <>
+      <Helmet>
+        <title>R/E-Commerce</title>
+      </Helmet>
       <ErrorBoundary>
         <Suspense fallback={<h1>Loading...</h1>}>
           <DataHooks />
