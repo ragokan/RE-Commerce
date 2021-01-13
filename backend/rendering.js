9f0720e5d9bb442022faa5f2744253302ea76b7f
Rago <63511519+ragokan@users.noreply.github.com>
2021-01-13 21:48:48 +0300

    renderingjs

--- backend/rendering.js
+++ backend/rendering.js
@@ -3,7 +3,7 @@ import path from "path";
 export default (app, express) => {
   if (process.env.NODE_ENV === "production") {
     var __dirname = path.resolve();
-    app.use(express.static("frontend/build"));
+    app.use(express.static(__dirname + "frontend/build"));
 
     app.get("*", (req, res) => {
       res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
